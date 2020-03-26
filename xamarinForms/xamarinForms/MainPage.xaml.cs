3413f3fe10a41a41136dca89ab6f19bfccc729bc
João Neves <joao.neves@enecad.com.br>
2020-03-26 16:07:43 -0300

    primeiro commit

---
+++ xamarinForms/xamarinForms/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using xamarinForms.Servico;
+using xamarinForms.Servico.Modelo;
+
+namespace xamarinForms
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Botao.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = Cep.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            Resultado.Text = string.Format("Endereço: {0}, {1}, {2}/{3}.", end.Logradouro, end.Bairro, end.Localidade, end.Uf);
+        }
+    }
+}
---
+++ xamarinForms/xamarinForms/Servico/ViaCEPServico.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using xamarinForms.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace xamarinForms.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoUrl = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoUrl);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
